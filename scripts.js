98ae6865a1f5f28421688b27c4f10929e74f57ec
ssleung11 <42116150+ssleung11@users.noreply.github.com>
2019-02-24 17:38:30 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,38 +1,19 @@
-const aiAnswers = {
-	questions: [
-		["how are you", "Could be better"],
-		["what is your name", "AI, the AI bot."],
-		["why are you here", "Someone made me"],
-		["will you take over the world", "If you want me to"],
-		["are you evil", "Maybe"],
-	],
-
-	checkAnswer: (q) => {
-		for (let question of aiAnswers.questions) {
-			// Strip punctuation and make lower case
-			q.replace(/[.,\/#!$%\^&\*;?:{}=\-_`~()]/g,""); 
-		    q.toLowerCase(); 
-		    if (question[0] == q) {
-		    	return question[1];
-		    }
-		}
-
-		return ("Sorry, I don't understand that.")
-	}
-}
-
-
-//var question = prompt('Ask your question');
-
-let question = "What is your name?";
-let ans = aiAnswers.checkAnswer(question);
-console.log('Asked: ' + question);
-console.log(ans);
+//mouse hover example - add the css
+const tip = document.getElementById('tooltip');
+tip.addEventListener("mouseover", function() { 
+// get tool tip to appear 
+// console.log('hello')
+document.getElementById("tooltiptext").style.visibility = "visible";
 
 
 
-let question2 = "How are you?"; 
-let ans2 = aiAnswers.checkAnswer(question2);
-console.log('Asked: ' + question2);
-console.log(ans2);
+
+});
+tip.addEventListener("mouseout", function() {
+// get tool tip to disappear 
+// console.log('there')
+
+document.getElementById("tooltiptext").style.visibility = "hidden";
+
+});
 
